24ee45615c2faa0b899617350b68d9d3297652cf
IceCreamZXZ <94557788+IceCreamZXZ@users.noreply.github.com>
2026-09-07 11:42:00 +0200

    feat: scanner ordner einlesen (BE-01) (#38)
    * feat: read incoming pdfs from inbox
    
    * feat: move file to destination dir based on confidence
    
    * dev: remove unnecessary directory
    
    * fix: linter errors
    
    * fix: translate error messages to english
    
    * fix: translate missing error message to english

---
+++ apps/bff/src/services/ScannerReaderService.js
@@ -0,0 +1,77 @@
+import fs from "node:fs";
+import fsPromises from "node:fs/promises";
+import path from "node:path";
+
+export default class ScannerReaderService {
+  constructor(directory) {
+    this.directory = directory;
+    this.watcher = null;
+  }
+
+  startObserver(onPdf) {
+    this.watcher = fs.watch(this.directory, async (eventType, filename) => {
+      if (!filename) {
+        return;
+      }
+
+      if (path.extname(filename).toLowerCase() !== ".pdf") {
+        return;
+      }
+
+      if (eventType !== "rename") {
+        return;
+      }
+
+      const filePath = path.join(this.directory, filename);
+
+      try {
+        await fsPromises.access(filePath, fsPromises.constants.F_OK);
+      } catch {
+        return;
+      }
+
+      try {
+        await this.waitForFile(filePath);
+
+        const pdfBuffer = await fsPromises.readFile(filePath);
+        const file = new File([pdfBuffer], filePath, { type: "application/pdf" });
+
+        await onPdf(file, filename.substring(0, filename.length - 4));
+      } catch (error) {
+        console.log(`Error processing ${filename}`, error);
+      }
+    });
+    console.log("Observer started");
+  }
+
+  async waitForFile(filePath, retries = 10) {
+    for (let i = 0; i < retries; i++) {
+      try {
+        await fsPromises.access(filePath);
+
+        const size1 = (await fsPromises.stat(filePath)).size;
+
+        await new Promise((resolve) => setTimeout(resolve, 500));
+
+        const size2 = (await fsPromises.stat(filePath)).size;
+
+        if (size1 === size2) {
+          return;
+        }
+      } catch (error) {
+        console.error(`Error waiting for file ${filePath}:`, error);
+      }
+
+      await new Promise((resolve) => setTimeout(resolve, 500));
+    }
+
+    throw new Error(`File was not made available in time: ${filePath}`);
+  }
+
+  stopObserver() {
+    if (this.watcher) {
+      this.watcher.close();
+      this.watcher = null;
+    }
+  }
+}

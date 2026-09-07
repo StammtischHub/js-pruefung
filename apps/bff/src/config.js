16ae53c0f158accdd1322fe1d7e3ff6c61ebb181
Leon Reusch <leon@reusch-home.de>
2026-09-07 15:01:01 +0200

    refactor: rename processing directory to processed

--- apps/bff/src/config.js
+++ apps/bff/src/config.js
@@ -14,7 +14,7 @@ export const config = {
     scanner: path.join(projectRoot, "data", "scanner"),
     inbox: path.join(projectRoot, "data", "inbox"),
     waiting: path.join(projectRoot, "data", "waiting"),
-    processing: path.join(projectRoot, "data", "processing"),
+    processing: path.join(projectRoot, "data", "processed"),
     trash: path.join(projectRoot, "data", "trash"),
   },
 

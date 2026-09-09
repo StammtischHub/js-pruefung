8697a6973d9b74c9ea90be2cd1d51deaf86dfaf0
Leon Reusch <leon@reusch-home.de>
2026-09-09 21:51:47 +0200

    chore: run formatter

--- eslint.config.js
+++ eslint.config.js
@@ -36,7 +36,7 @@ export default [
       "unused-imports/no-unused-imports": "error",
       "unused-imports/no-unused-vars": [
         "warn",
-        { vars: "all", varsIgnorePattern: "^_", args: "after-used", argsIgnorePattern: "^_" }
+        { vars: "all", varsIgnorePattern: "^_", args: "after-used", argsIgnorePattern: "^_" },
       ],
     },
   },

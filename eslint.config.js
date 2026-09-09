b51c710bbd753d3c269642fa390caaad2ddb36df
Leon R. <94672224+Leon4ever2802@users.noreply.github.com>
2026-09-09 21:52:49 +0200

    chore: add eslint plugin for unused imports (#47)

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,13 @@
 import js from "@eslint/js";
 import globals from "globals";
 import prettierConfig from "eslint-config-prettier";
+import unusedImports from "eslint-plugin-unused-imports";
 
 export default [
   js.configs.recommended,
 
   {
-    files: ["apps/bff/**/*.js"],
+    files: ["apps/bff/**/*.js", "eslint.config.js"],
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
@@ -28,13 +29,15 @@ export default [
   },
 
   {
-    files: ["eslint.config.js"],
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      globals: {
-        ...globals.node,
-      },
+    files: ["**/*.js"],
+    plugins: { "unused-imports": unusedImports },
+    rules: {
+      "no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        { vars: "all", varsIgnorePattern: "^_", args: "after-used", argsIgnorePattern: "^_" },
+      ],
     },
   },
 

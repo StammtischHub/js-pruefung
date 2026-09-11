3ba322515f14eb8467e2eabb46519473a0de3dd2
Jannis <j_dickel@web.de>
2026-09-11 19:28:31 +0200

    refactor: adapt naming to be more clearly

--- apps/bff/src/services/sortingService.js
+++ apps/bff/src/services/sortingService.js
@@ -5,11 +5,11 @@ export async function next() {
   const inInbox = await getDocumentsByState(State.INBOX)
   if (inInbox.length === 0) { return null }
 
-  const oldestDocument = inInbox.sort((a, b) => {
+  const sortedDocuments = inInbox.sort((a, b) => {
     const aDate = new Date(a.creationDate).getTime();
     const bDate = new Date(b.creationDate).getTime();
     return aDate - bDate;
   });
 
-  return oldestDocument[0];
+  return sortedDocuments[0];
 }

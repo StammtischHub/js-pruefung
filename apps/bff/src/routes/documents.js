63e85942bb33a2aaa9a287ea39377485dbbc2bad
Leon R. <94672224+Leon4ever2802@users.noreply.github.com>
2026-09-11 12:47:14 +0200

    feat: BE-03 document endpoint (#49)

--- apps/bff/src/routes/documents.js
+++ apps/bff/src/routes/documents.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { pdfUpload } from "../services/pdfUpload.js";
 import { Document } from "../objects/Document.js";
+import { getAllMetadata } from "../storage/metadataStore.js";
 
 const router = express.Router();
 
@@ -30,6 +31,20 @@ router.post("/", (req, res) => {
       return res.status(500).json({ error: "Processing failed." });
     }
   });
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const state = req.query.state;
+
+    if (!state) return res.status(400).json({ error: "No 'state' query provided." });
+
+    const metadata = await getAllMetadata(state.toUpperCase());
+    return res.status(200).json(metadata.map((document) => document.path));
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: "Fetching documents failed." });
+  }
 });
 
 export default router;
--- apps/bff/src/storage/metadataStore.js
+++ apps/bff/src/storage/metadataStore.js
@@ -47,6 +47,10 @@ export async function updateDocumentInMetadata(document) {
   });
 }
 
-export async function getAllMetadata() {
-  return enqueue(() => readAll());
+export async function getAllMetadata(state = undefined) {
+  return enqueue(() =>
+    readAll().then((metadata) => {
+      return state ? metadata.filter((document) => document.state === state) : metadata;
+    })
+  );
 }
